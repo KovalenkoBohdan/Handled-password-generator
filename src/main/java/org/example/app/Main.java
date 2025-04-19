06e10a18b404f21bba0b925cddca77b8582bdb6a
KovalenkoBogdan <kbohdan45@gmail.com>
2025-04-19 19:04:50 +0300

    Add Scanner

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,11 +1,20 @@
 package org.example.app;
 
+import java.util.Scanner;
+
 import static org.example.app.PasswordGenerator.generatePassword;
 
 public class Main {
     public static void main(String[] args) {
-        int passwordLength = 10;
-        String generatedPassword = generatePassword(passwordLength);
-        System.out.println("Згенерований пароль: " + generatedPassword);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Ведіть довжину пароля: ");
+            int passwordLength = scanner.nextInt();
+            String generatedPassword = generatePassword(passwordLength);
+            System.out.println("Згенерований пароль: " + generatedPassword);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Невідома помилка: " + e.getMessage());
+        }
     }
 }
